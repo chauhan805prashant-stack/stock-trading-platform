1db5b6ba64638976523a988500c6660a505b2433
Prashant Chauhan <chauhan805prashant@gmail.com>
2026-02-20 01:33:09 +0530

    NAVBAR,FOOTER AND OPENACCOUNT

--- frontend/src/landing_page/openaccount.js
+++ frontend/src/landing_page/openaccount.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function openaccount() {
     return ( 
-        <h1>openaccount</h1>
+         <div className='container p-5'>
+            <div className='row text-center'>
+                <h1 className='mt-5'>Open a Zerodha Account</h1>
+                <p>Modern Platform and Apps,<i class="fa fa-inr" aria-hidden="true"></i> 0 ivestement and flat <i class="fa fa-inr" aria-hidden="true"></i> 20 infraday and F&O trades.</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Sign up Now</button>
+            </div>
+
+        </div>
      );
 }
 
